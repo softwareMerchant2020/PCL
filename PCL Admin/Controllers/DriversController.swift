4e4422e5f773cc153da4aa319a18ecd506b10290
Rutul <rutuldesai1993@gmail.com>
2020-04-13 11:27:01 -0400

    Added Rest Manager to the project.

--- PCL Admin/Controllers/DriversController.swift
+++ PCL Admin/Controllers/DriversController.swift
@@ -37,14 +37,4 @@ class DriversController: UIViewController, UITableViewDelegate, UITableViewDataS
         self.dismiss(animated: true, completion: nil)
         
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
